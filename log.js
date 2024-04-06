8542820013f1df43e984996f0cd0745b02d7ef75
AlexisAnderson <alexis.k.anderson@gmail.com>
2024-04-05 23:50:05 -0400

    Checking array values

--- log.js
+++ log.js
@@ -25,3 +25,11 @@ function TravelLog() {
         }
     }
 }
+
+function getInfo() {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            console.log(arr[i][j]);
+        }
+    }
+}
