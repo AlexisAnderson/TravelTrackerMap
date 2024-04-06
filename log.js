ec7f837363405820b6f9a17f8af9571493cbe11d
AlexisAnderson <alexis.k.anderson@gmail.com>
2024-04-05 23:39:55 -0400

    Adding submission action

--- log.js
+++ log.js
@@ -4,34 +4,21 @@ arr[0] = [];
 arr[1] = [];
 arr[2] = [];
 
-let Tylogging = TravelLog("ty", "USA");
-let Allogging = TravelLog("alexis", "UK");
-let Kenlogging = TravelLog("kenny", "Canada");
-let Tylogging2 = TravelLog("Ty", "UK");
-let Allogging2 = TravelLog("Alexis", "Maldives");
-let kenlogging2 = TravelLog("Kenny", "France");
-
-getInfo();
-
-function getInfo() {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            console.log(arr[i][j]);
-        }
-    }
-}
-
-function TravelLog(name, input) {
-    let Input = input.toLowerCase();
+function TravelLog() {
+    let name = document.getElementById("Name").value;
+    let country = document.getElementById("Country").value;
+    let Input = country.toLowerCase();
     let Name = name.toLowerCase();
     for (let i = 0; i < arr.length; i++) {
         if (arr[i][0] == Name) {
             arr[i].push(Input);
+            window.alert("Thank you "+arr[i][0]+" for your country submission of "+arr[i][1]);
             break;
         }
         if (arr[i][0] != Name && arr[i].length == 0) {
             arr[i][0] = Name
             arr[i].push(Input);
+            window.alert("Thank you "+arr[i][0]+" for your country submission of "+arr[i][1]);
             break;
         } else {
             continue;
